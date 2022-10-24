60b62f0b91b09e469ba0e9630674e76c4ec83c89
Osuji Augustine Ifeanyi <101365788+Augustine56@users.noreply.github.com>
2022-10-24 02:31:42 +0100

    fourth commit

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,10 +1,100 @@
 import React from 'react'
 import './Services.css'
+import {BiCheck} from 'react-icons/bi'
 
 const Services = () => {
   return (
     <section id="services">
-       My services page
+       <h5> What I Offer</h5>
+       <h2>My Services</h2>
+
+       <div className='container services__container'>
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Web Development</h3>
+          </div>
+          <ul className='service__list'>
+            <li> 
+              <BiCheck className='service__list-icon'/>
+            <p>Website application from scratch</p>
+            </li>
+            <li> 
+              <BiCheck className='service__list-icon'/>
+            <p>Website application from scratch</p>
+            </li>
+            <li> 
+              <BiCheck className='service__list-icon'/>
+            <p>Website application from scratch</p>
+            </li>
+            <li> 
+              <BiCheck className='service__list-icon'/>
+            <p>Website application from scratch</p>
+            </li>
+            <li> 
+              <BiCheck className='service__list-icon'/>
+            <p>Website application from scratch</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF WEB Development */}
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Mobile App Development</h3>
+          </div>
+          <ul className='service__list'>
+            <li> 
+              <BiCheck className='service__list-icon'/>
+            <p>Mobile application from scratch</p>
+            </li>
+            <li> 
+              <BiCheck className='service__list-icon'/>
+            <p>Mobile application from scratch</p>
+            </li>
+            <li> 
+              <BiCheck className='service__list-icon'/>
+            <p>Mobile application from scratch</p>
+            </li>
+            <li> 
+              <BiCheck className='service__list-icon'/>
+            <p>Mobile application from scratch</p>
+            </li>
+            <li> 
+              <BiCheck className='service__list-icon'/>
+            <p>Mobile application from scratch</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF MOBILE APPLICATION */}
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Content Creation</h3>
+          </div>
+          <ul className='service__list'>
+            <li> 
+              <BiCheck className='service__list-icon'/>
+            <p>Create new ideas</p>
+            </li>
+            <li> 
+              <BiCheck className='service__list-icon'/>
+            <p>Create new ideas</p>
+            </li>
+            <li> 
+              <BiCheck className='service__list-icon'/>
+            <p>Create new ideas</p>
+            </li>
+            <li> 
+              <BiCheck className='service__list-icon'/>
+            <p>Create new ideas</p>
+            </li>
+            <li> 
+              <BiCheck className='service__list-icon'/>
+            <p>Create new ideas</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF CONTENT CREATION */}
+
+       </div>
     </section>
   )
 }
