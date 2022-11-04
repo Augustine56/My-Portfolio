45272157a0fb5a9c6048e5736ef11af07445bfd4
Osuji Augustine Ifeanyi <101365788+Augustine56@users.noreply.github.com>
2022-11-04 10:08:12 +0100

    fifth commit

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './Portfolio.css'
+// import Weather from '../../pictures/weather app icon1.jpg'
 
 const Portfolio = () => {
   return (
     <section id="portfolio">
-       Portfolio page
+       <h5>My Recent  Works</h5>
+       <h2>Portfolio</h2>
+       
+       <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            {/* <Weather/> */}
+          </div>
+          <h3>Weather App</h3>
+          <a href='https://weatherapi202.netlify.app' className='btn btn-primary' rel="noreferrer" target='_blank'>Live Demo</a>
+
+
+
+
+        </article>
+       </div>
     </section>
   )
 }
