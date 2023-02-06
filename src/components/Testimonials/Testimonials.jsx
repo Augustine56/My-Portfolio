8604e4093b70a5b2c8fcde104753b5ddbd62d1ef
Osuji Augustine Ifeanyi <101365788+Augustine56@users.noreply.github.com>
2023-02-06 15:59:55 +0100

    sixth commit

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,10 +1,78 @@
-import React from 'react'
+ import React from 'react'
 import './Testimonials.css'
+import AVTR1 from '../../pictures/mypic.jpg'
+import AVTR2 from '../../pictures/mypic7.jpg'
+import AVTR3 from '../../pictures/mypic.jpg'
+import AVTR4 from '../../pictures/mypic7.jpg'
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+
+
+const data = [
+  {
+    avatar:AVTR1,
+    name: 'Augustine',
+    review:'This is a wonderful Portfolio, really beautiful and awesome, great Job. would like working with you in the future, this is a great React project.'
+              
+  },
+  {
+    avatar:AVTR2,
+    name: 'Ifeanyi',
+    review:'This is a wonderful Portfolio, really beautiful and awesome, great Job. would like working with you in the future, this is a great React project.' 
+  },
+  {
+    avatar:AVTR3,
+    name: 'Augustine',
+    review:'This is a wonderful Portfolio, really beautiful and awesome, great Job. would like working with you in the future, this is a great React project.'
+  },
+  {
+    avatar:AVTR4,
+    name: 'Ifeanyi',
+    review:'This is a wonderful Portfolio, really beautiful and awesome, great Job. would like working with you in the future, this is a great React project.'
+  },
+  
+  ]
 
 const Testimonials = () => {
   return (
     <section id="testimonials">
-       Testimonials page
+    <h5>Review from clients</h5>
+    <h2>Testimonials</h2>
+
+    <Swiper className="container testimonials_container"
+      // install Swiper modules
+      modules={[ Pagination ]}
+      spaceBetween={30}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}>
+      {
+        data.map(({avatar, name, review}, index) => {
+          return(
+            <SwiperSlide key={index} className="testimonial">
+        <div className="client_avatar">
+          <img src={avatar} alt="avatar"/>
+          </div>
+          <h5 className='client_name'>{name}</h5>
+          <small  className="client_review">{review}</small>
+
+
+     
+      </SwiperSlide>
+          )
+        })
+      }
+    </Swiper>
     </section>
   )
 }
